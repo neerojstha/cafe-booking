aee48399427e4559c4719e9edb6392ca38cb01e7
neerojstha <nirojstha@hotmail.com>
2023-08-06 14:29:54 +0000

    django models created

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+ 
+class Post(models.Model):
+    title = models.CharField(max_length=210, unique=True)
+    slug = models.SlugField(max_length=210, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+    
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_people = models.PositiveIntegerField()
+    comments = models.TextField(blank=True)
+    canceled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.time}"
+
+    def clean(self):
+        # Check for double booking
+        if self.pk is None:  # Only check for new instances (not updating)
+            conflicting_bookings = Booking.objects.filter(
+                date=self.date,
+                time=self.time,
+                canceled=False
+            )
+            if conflicting_bookings.exists():
+                raise ValidationError("This time slot is already booked.")
+
+    def cancel_booking(self):
+        if not self.canceled:
+            self.canceled = True
+            self.save()
+
+    def is_cancellable(self):
+        return not self.canceled
+
+
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    table_limit = models.IntegerField()
+
+class Menu(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
--- ediary/settings.py
+++ ediary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '8000-neerojstha-cafebooking-xjk9r5g6e3x.ws-eu102.gitpod.io' 'Localhost']
+ALLOWED_HOSTS = ['8000-neerojstha-cafebooking-xjk9r5g6e3x.ws-eu102.gitpod.io', 'localhost']
 
 
 # Application definition
