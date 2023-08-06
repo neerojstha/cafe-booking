84dcb68ccc62c1e0ece891fdf5389f4b3361bcb8
neerojstha <nirojstha@hotmail.com>
2023-08-06 15:41:10 +0000

    base.html page created

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post, Menu, Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = 'content'
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    list_filter  = ('name', 'price')
+    search_fields = ('name', 'description')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'date')
+    list_filter = ('name', 'email', 'date')
+
