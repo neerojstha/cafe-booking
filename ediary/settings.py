3e93a991a6e506a93e3fcda9fe2b8c5f663ab54e
neerojstha <nirojstha@hotmail.com>
2023-08-07 09:59:49 +0000

    deployment commit

--- ediary/settings.py
+++ ediary/settings.py
@@ -27,7 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-neerojstha-cafebooking-xjk9r5g6e3x.ws-eu102.gitpod.io', 'localhost']
 
